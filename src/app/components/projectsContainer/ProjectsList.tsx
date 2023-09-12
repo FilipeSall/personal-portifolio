dc8181091d2c6a606ec66f91f493b8c51342cc04
Filipe Salles <filipesalles69@gmail.com>
2023-09-12 18:28:40 -0300

    agora vai pt2

--- src/app/components/projectsContainer/ProjectsList.tsx
+++ src/app/components/projectsContainer/ProjectsList.tsx
@@ -5,11 +5,11 @@ import initialPhoto from '../../../../public/linktaskweather/initialphoto.webp'
 
 import initialPhotoOpus from '../../../../public/opusproject/initialPhoto.webp';
 import googleStatusImgOpus from '../../../../public/opusproject/googleStatusImg.webp';
-import opusimg1 from '../../../../public/opusproject/opusImg1.webp';
-import opusimg2 from '../../../../public/opusproject/opusImg2.webp';
-import opusimg3 from '../../../../public/opusproject/opusImg3.webp';
-import opusimg4 from '../../../../public/opusproject/opusImg4.webp';
-import opusimg5 from '../../../../public/opusproject/opusImg5.webp';
+import opusimg1 from '../../../../public/opusproject/opusimg1.webp';
+import opusimg2 from '../../../../public/opusproject/opusimg2.webp';
+import opusimg3 from '../../../../public/opusproject/opusimg3.webp';
+import opusimg4 from '../../../../public/opusproject/opusimg4.webp';
+import opusimg5 from '../../../../public/opusproject/opusimg5.webp';
 
 import initialPhotoQuizz from '../../../../public/quizzgame/initialPoto.webp';
 import googleStatusImgQuizzgame from '../../../../public/quizzgame/googleStatusImg.webp';
